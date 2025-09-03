92a05b892d5d121a4cce4511a5d04f321d63f723
okit-man <126726077+okit-man@users.noreply.github.com>
2025-09-03 04:09:59 +0100

    added operations to the button

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
+// set initial count
 let count = 0
+
+// select both value and buttons 
 const countValue = document.querySelector('#value')
 const btns =  document.querySelectorAll('.btn')
 
 btns.forEach(function (btn) {
     btn.addEventListener('click', function(e) {
-        console.log(e.currentTarget.classList);
+        // console.log(e.currentTarget.classList);
         
+        const styles = e.currentTarget.classList
+        if (styles.contains('decrease')) {
+            count--
+        } else if (styles.contains('increase')) {
+            count++
+        } else {
+            count = 0
+        }
+        if (count > 0) {
+            countValue.style.color = ('rgb(18, 28, 211)')
+        }else if (count < 0) {
+            countValue.style.color = (' rgb(200,25,25)')
+        }
+        else  {
+            countValue.style.color = ('rgb(0,0,0')
+        }
+        countValue.textContent = count
+       
     })
 })
